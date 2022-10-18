0b46d94db1a9e7b1390d052c41ef73ab68392683
AshaAbdu <halge87@yahoo.com>
2022-10-17 19:31:43 -0500

    last commit of wk13- Open API

---
+++ src/main/java/com/promineotech/jeep/controller/BasicJeepSalesController.java
@@ -0,0 +1,19 @@
+package com.promineotech.jeep.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.promineotech.jeep.entity.Jeep;
+import com.promineotech.jeep.entity.JeepModel;
+
+@RestController 
+public class BasicJeepSalesController implements JeepSalesController {
+
+	@Override
+	public List<Jeep> fetchJeeps(JeepModel model, String trim) {
+	
+		return null;
+	}
+
+}
