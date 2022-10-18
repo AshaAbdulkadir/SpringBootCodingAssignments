a0003754b95e06dbb7b7de1cc46a1cb61d50f343
AshaAbdu <halge87@yahoo.com>
2022-10-18 00:11:36 -0500

    W14- component scan

--- src/main/java/com/promineotech/jeep/JeepSales.java
+++ src/main/java/com/promineotech/jeep/JeepSales.java
@@ -3,7 +3,7 @@ package com.promineotech.jeep;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.promineotech"})
 public class JeepSales {
 
 	public static void main(String[] args) {
