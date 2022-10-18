95fc0edc23837e48ceff83ef6a3a59718b8aa0c1
AshaAbdu <halge87@yahoo.com>
2022-10-17 23:59:42 -0500

    controller scan and Lombok

--- src/main/java/com/promineotech/jeep/controller/BasicJeepSalesController.java
+++ src/main/java/com/promineotech/jeep/controller/BasicJeepSalesController.java
@@ -7,12 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
 
-@RestController 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+@Slf4j
 public class BasicJeepSalesController implements JeepSalesController {
+	 Logger log = LoggerFactory.getLogger(getClass());
 
 	@Override
 	public List<Jeep> fetchJeeps(JeepModel model, String trim) {
-	
+		log.info("model={}, trim={}", model, trim);
 		return null;
 	}
 
--- src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
+++ src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
@@ -1,39 +1,51 @@
 package com.promineotech.jeep.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
 
-import com.promineotech.jeep.controller.support.FetchJeepTestSupport;
 import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
 
+import lombok.Getter;
+
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-class FetchJeepTest extends FetchJeepTestSupport{
+class FetchJeepTest {
 
 	@Test
 	void testThatJeepsAreReturnedWhenAValidModelAndTrimAreSupplied() {
-		//Given a valid model, trim and URI
+		// Given a valid model, trim and URI
 		JeepModel model = JeepModel.WRANGLER;
-		String trim ="Sport";
+		String trim = "Sport";
 		String uri = 
-				String.format("%s?model=%s&trim=%s", getBaseUri(), model, trim);
+				String.format("http://localhost:%d/jeeps?model=%s&trim=%s", serverPort, model, trim);
 		
-		//when: a connection is made to the URI
-		ResponseEntity<List<Jeep>> response = 
-				getRestTemplate().exchange(uri, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+		System.out.println(uri);
 		
-		//Then: a success(OK - 200) is returned 
+		// When: a connection is made to the URI
+		ResponseEntity<List<Jeep>> response = restTemplate.exchange(uri, HttpMethod.GET, null, 
+				new ParameterizedTypeReference<>() {});
+		
+		// Then: a success (OK - 200) status code is returned
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
+	@Autowired
+	@Getter
+	private TestRestTemplate restTemplate;
+	
+	@LocalServerPort
+	private int serverPort;
 
 }
