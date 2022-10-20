66e2efb98906ffa13e5e76ffe4ee8d9dd543fc65
AshaAbdu <halge87@yahoo.com>
2022-10-20 00:38:03 -0500

    wk15-Mockito

--- src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
+++ src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
@@ -6,14 +6,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.mockito.Mockito.doThrow;
+
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
@@ -28,40 +33,98 @@ import com.promineotech.jeep.Constants;
 import com.promineotech.jeep.controller.support.FetchJeepTestSupport;
 import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
+import com.promineotech.jeep.service.JeepSalesService;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
-@Sql(scripts = { "classpath:flyway/migrations/V1.0__Jeep_Schema.sql",
-		"classpath:flyway/migrations/V1.1__Jeep_Data.sql" }, config = @SqlConfig(encoding = "utf-8"))
 
-class FetchJeepTest extends FetchJeepTestSupport {
-	
+
+class FetchJeepTest {
 	@Autowired
 	private TestRestTemplate restTemplate;
 
 	@LocalServerPort
 	private int serverPort;
 	
-	@ParameterizedTest
-	@MethodSource("com.promineotech.jeep.controller.FetchJeepTest#parametersForInvalidInput")
-	void testThatAnErrorMessageIsReturnedWhenAnInvalidValueIsSupplied(String model, String trim, String reason) {
-		
-		// Given: a valid model, trim and URI
+	@Nested
+	@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+	@ActiveProfiles("test")
+	@Sql(scripts = { "classpath:flyway/migrations/V1.0__Jeep_Schema.sql",
+			"classpath:flyway/migrations/V1.1__Jeep_Data.sql" }, config = @SqlConfig(encoding = "utf-8"))
+	class TestsThatDoNotPolluteTheApplicationContext extends FetchJeepTestSupport {
+		/*
+		 * 
+		 */
+		@ParameterizedTest
+		@MethodSource("com.promineotech.jeep.controller.FetchJeepTest#parametersForInvalidInput")
+		void testThatAnErrorMessageIsReturnedWhenAnInvalidValueIsSupplied(String model, String trim, String reason) {
+			
+			// Given: a valid model, trim and URI
 
-		String uri = String.format("http://localhost:%d/jeeps?model=%s&trim=%s", 
-			       serverPort, model, trim);
+			String uri = String.format("http://localhost:%d/jeeps?model=%s&trim=%s", 
+				       serverPort, model, trim);
+			
+			// When: a connection is made to the URI
+			ResponseEntity<Map<String, Object>> response = getRestTemplate().exchange(uri,
+					 HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+			
+			// Then: a not found (404) status code is returned
+			assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+			
+			// And: an error message is returned 
+			Map<String, Object> error = response.getBody();
+			
+			assertErrorMessageValid(error, HttpStatus.BAD_REQUEST);
+		}
 		
-		// When: a connection is made to the URI
-		ResponseEntity<Map<String, Object>> response = getRestTemplate().exchange(uri,
-				 HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+		/*
+		 * 
+		 */
+		@Test
+		void testThatAnErrorMessageIsReturnedWhenAnUnknownTrimIsSupplied() {
+			
+			// Given: a valid model, trim and URI
+			JeepModel model = JeepModel.WRANGLER;
+			String trim = "Unknown Value";
+			String uri = String.format("http://localhost:%d/jeeps?model=%s&trim=%s", serverPort, model, trim);
+			
+			// When: a connection is made to the URI
+			ResponseEntity<Map<String, Object>> response = getRestTemplate().exchange(
+					uri, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+			
+			// Then: a not found (404) status code is returned
+			assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+			
+			// And: an error message is returned 
+			Map<String, Object> error = response.getBody();
+			
+			assertErrorMessageValid(error, HttpStatus.NOT_FOUND);
+		}
 		
-		// Then: a not found (404) status code is returned
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+		/*
+		 * 
+		 */
+		@Test
+		void testThatJeepsAreReturnedWhenAValidModelAndTrimAreSupplied() {
+
+			// Given: a valid model, trim and URI
+			JeepModel model = JeepModel.WRANGLER;
+			String trim = "Sport";
+			String uri = String.format("http://localhost:%d/jeeps?model=%s&trim=%s", serverPort, model, trim);
+
+			// When: a connection is made to the URI
+			ResponseEntity<List<Jeep>> response = restTemplate.exchange(uri, HttpMethod.GET, null,
+					new ParameterizedTypeReference<>() {
+					});
+
+			// Then: a success (OK - 200) status code is returned
+			assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+			// And: the actual list is the same as the expected list
+			List<Jeep> actual = response.getBody();
+			List<Jeep> expected = buildExpected();
+
+			assertThat(actual).isEqualTo(expected);
+		}
 		
-		// And: an error message is returned 
-		Map<String, Object> error = response.getBody();
-		
-		assertErrorMessageValid(error, HttpStatus.BAD_REQUEST);
 	}
 	
 	static Stream<Arguments> parametersForInvalidInput(){
@@ -74,50 +137,43 @@ class FetchJeepTest extends FetchJeepTestSupport {
 		);
 	}
 	
-	@Test
-	void testThatAnErrorMessageIsReturnedWhenAnUnknownTrimIsSupplied() {
+	
+	@Nested
+	@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+	@ActiveProfiles("test")
+	@Sql(scripts = { "classpath:flyway/migrations/V1.0__Jeep_Schema.sql",
+			"classpath:flyway/migrations/V1.1__Jeep_Data.sql" }, config = @SqlConfig(encoding = "utf-8"))
+	class TestsThatPolluteTheApplicationContext extends FetchJeepTestSupport {
 		
-		// Given: a valid model, trim and URI
-		JeepModel model = JeepModel.WRANGLER;
-		String trim = "invalid Value";
-		String uri = String.format("http://localhost:%d/jeeps?model=%s&trim=%s", 
-			       serverPort, model, trim);
+		@MockBean
+		private JeepSalesService jeepSalesService;
 		
-		// When: a connection is made to the URI
-		ResponseEntity<Map<String, Object>> response = getRestTemplate().exchange(
-				uri, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+		/*
+		 * 
+		 */
+		@Test
+		void testThatAnUnplannedErrorResultsInA500Status() {
+			
+			// Given: a valid model, trim and URI
+			JeepModel model = JeepModel.WRANGLER;
+			String trim = "Invalid";
+			String uri = String.format("http://localhost:%d/jeeps?model=%s&trim=%s", serverPort, model, trim);
+			
+			doThrow(new RuntimeException("Ouch")).when(jeepSalesService).fetchJeeps(model, trim);
+			
+			// When: a connection is made to the URI
+			ResponseEntity<Map<String, Object>> response = getRestTemplate().exchange(
+					uri, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+			
+			// Then: an internal server error (500) status is returned
+			assertThat(response.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+			
+			// And: an error message is returned 
+			Map<String, Object> error = response.getBody();
+			
+			assertErrorMessageValid(error, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		
-		// Then: a not found (404) status code is returned
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-		
-		// And: an error message is returned 
-		Map<String, Object> error = response.getBody();
-		
-		assertErrorMessageValid(error, HttpStatus.NOT_FOUND);
 	}
-
-	@Test
-	void testThatJeepsAreReturnedWhenAValidModelAndTrimAreSupplied() {
-
-		// Given: a valid model, trim and URI
-		JeepModel model = JeepModel.WRANGLER;
-		String trim = "Sport";
-		String uri = String.format("http://localhost:%d/jeeps?model=%s&trim=%s", serverPort, model, trim);
-
-		// When: a connection is made to the URI
-		ResponseEntity<List<Jeep>> response = restTemplate.exchange(uri, HttpMethod.GET, null,
-				new ParameterizedTypeReference<>() {
-				});
-
-		// Then: a success (OK - 200) status code is returned
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-
-		// And: the actual list is the same as the expected list
-		List<Jeep> actual = response.getBody();
-		List<Jeep> expected = buildExpected();
-
-		assertThat(actual).isEqualTo(expected);
-	}
-
 
 }
