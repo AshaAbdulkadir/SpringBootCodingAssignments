edbf3d7d2d1ed7e11176a23ee82bcf14c7b2bbc0
AshaAbdu <halge87@yahoo.com>
2022-10-23 01:00:08 -0500

    successful image upload test

--- src/test/java/com/promineotech/jeep/controller/ImageUploadTest.java
+++ src/test/java/com/promineotech/jeep/controller/ImageUploadTest.java
@@ -1,7 +1,11 @@
 package com.promineotech.jeep.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.InputStream;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +13,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,11 +28,30 @@ class ImageUploadTest {
 	private MockMvc mockMvc;
 	
 	@Test
-	void testThatTheServerReceivesAnImageAndReturnsAnOkResponse() {
+	void testThatTheServerReceivesAnImageAndReturnsAnOkResponse() throws Exception {
 		Resource image = new ClassPathResource(JEEP_IMAGE);
 		
 		if(!image.exists()) {
 			fail("Could not find %s", JEEP_IMAGE);
+			
+		try(InputStream inputStream = image.getInputStream()) {
+			MockMultipartFile file = new MockMultipartFile("image", JEEP_IMAGE, 
+					MediaType.TEXT_PLAIN_VALUE, inputStream);
+			
+			// @formatter: off
+			
+			MvcResult result = mockMvc
+					.perform(MockMvcRequestBuilders
+					.multipart("/jeeps/1/image")
+					.file(file))
+					.andDo(print())
+					.andExpect(status().isOk())
+					.andReturn();
+			// @formatter: on 
+			
+			String content = result.getResponse().getContentAsString();
+			assertThat(content).isNotEmpty();
+		}
 		}
 	}
 
